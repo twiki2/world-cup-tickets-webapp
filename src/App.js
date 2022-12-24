d63cb4cd49a64712868a3d036b123dac484890e5
medo2002917 <120728764+medo2002917@users.noreply.github.com>
2022-12-24 03:41:39 +0200

    added forget-password

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Contact } from "./pages/contact";
+import { Cart } from "./pages/cart/cart";
+import { ShopContextProvider } from "./context/shop-context";
+import ForgetPassword from "./components/forget-password";
+
+function App() {
+  return (
+    <div className="App">
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path='/forget-password' element={<ForgetPassword/>}/>
+          </Routes>
+        </Router>
+      </ShopContextProvider>
+    </div>
+  );
+}
+
+export default App;
