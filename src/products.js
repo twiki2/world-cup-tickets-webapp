eb249d4af89e86007f99e2bd2d6a3ded45741255
Youssef Ashraf <jo@mail.com>
2022-12-24 21:46:47 +0200

    Added signup

---
+++ src/products.js
@@ -0,0 +1,117 @@
+import product1 from "./assets/products/Match 1.jpeg";
+import product2 from "./assets/products/Match 2.jpeg";
+import product3 from "./assets/products/Qatar3.jpg";
+import product4 from "./assets/products/Match 3.jpeg";
+import product5 from "./assets/products/Match 4.jpeg";
+import product6 from "./assets/products/Qatar3.jpg";
+import product7 from "./assets/products/Match 5.jpeg";
+import product8 from "./assets/products/Match 6.jpeg";
+import product9 from "./assets/products/Qatar3.jpg";
+import product10 from "./assets/products/Match 7.jpeg";
+import product11 from "./assets/products/Match 8.jpeg";
+import product12 from "./assets/products/Qatar3.jpg";
+import product13 from "./assets/products/Qatar4.jpg";
+import product14 from "./assets/products/Qatar4.jpg";
+import product15 from "./assets/products/Qatar.jpg";
+import product16 from "./assets/products/Match.jpeg";
+
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Match 1: Netherlands VS USA",
+    price: 200.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Match 2: Argentina VS Australia",
+    price: 200.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Match 9: Winner of Match 1 VS Winner of Match 2",
+    price: 250.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Match 3: Japan VS Croatia",
+    price: 200.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Match 4: Brazil VS South Korea",
+    price: 200.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Match 10: Winner of Match 3 VS Winner of Match 4",
+    price: 250.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Match 5: England VS Senegal",
+    price: 200.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Match 6: France VS Poland",
+    price: 200.0,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "Match 11: Winner of Match 5 VS Winner of Match 6",
+    price: 250.0,
+    productImage: product9,
+  },
+  {
+    id: 10,
+    productName: "Match 7: Morocco VS Spain",
+    price: 200.0,
+    productImage: product10,
+  },
+  {
+    id: 11,
+    productName: "Match 8: Portugal VS Switzerland",
+    price: 200.0,
+    productImage: product11,
+  },
+  {
+    id: 12,
+    productName: "Match 12: Winner of Match 7 VS Winner of Match 8",
+    price: 250.0,
+    productImage: product12,
+  },
+  {
+    id: 13,
+    productName: "Semi Final 1: Winner of Match 9 VS Winner of Match 10",
+    price: 300.0,
+    productImage: product13,
+  },
+  {
+    id: 14,
+    productName: "Semi Final 2: Winner of Match 11 VS Winner of Match 12",
+    price: 300.0,
+    productImage: product14,
+  },
+  {
+    id: 15,
+    productName: "Third place match: Loser of Match 13 VS Loser of Match 14",
+    price: 400.0,
+    productImage: product15,
+  },
+  {
+    id: 16,
+    productName: "Final: Winner of Semi Final 1 VS Winner of Semi Final 2",
+    price: 500.0,
+    productImage: product16,
+  }
+   
+];
