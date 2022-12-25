e05516929ff3fe8514f36345176a5f8ade88fb8a
Mostafa Gamal <mostafa.2c6@gmail.com>
2022-12-25 21:23:43 +0200

    Linked front/back end

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import { Navbar } from "./components/navbar";
 import { Shop } from "./pages/shop/shop";
-import { Contact } from "./pages/contact";
+import * as ROUTES from "./constants/routes";
 import { Cart } from "./pages/cart/cart";
 import { ShopContextProvider } from "./context/shop-context";
 import ForgetPassword from "./components/forget-password";
@@ -15,19 +15,21 @@ import Login from './components/login';
 function App() {
   return (
     <div className="App">
+      <BrowserRouter></BrowserRouter>
       <ShopContextProvider>
         <Router>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Shop />} />
-            <Route path='/forget-password' element={<ForgetPassword/>}/>
-            <Route exact path="/balance" element={<BalancePage/>}/>
-            <Route path="/cart" element={<Cart />} />
-            <Route path='/Signup' element={<Signup/>} />
-            <Route path="/login" element={<Login/>} />
+            <Route exact path={ROUTES.LANDING} element={<Shop />} />
+            <Route path={ROUTES.FORGET_PASSWORD} element={<ForgetPassword/>}/>
+            <Route  path={ROUTES.BALANCE} element={<BalancePage/>}/>
+            <Route path={ROUTES.CART} element={<Cart />} />
+            <Route path={ROUTES.SIGN_UP} element={<Signup/>} />
+            <Route path={ROUTES.LOG_IN} element={<Login/>} />
           </Routes>
         </Router>
       </ShopContextProvider>
+      
     </div>
   );
 }
---
+++ src/constants/routes.js
@@ -0,0 +1,10 @@
+
+export const LANDING = '/';
+export const SIGN_UP = '/signup';
+export const LOG_IN = '/login';
+export const HOME = '/home';
+export const CART ='/cart';
+export const BALANCE = '/balance';
+export const ADMIN = '/admin';
+export const ADMIN_DETAILS = '/admin/:id';
+export const FORGET_PASSWORD = '/forget-password';
